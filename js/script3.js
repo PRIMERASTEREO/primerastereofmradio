d8b34b20365beaa05346a6b7385dfa6aefebbac7
PRIMERA STEREO Radio Online <primerastereo2020@gmail.com>
2025-09-17 17:10:24 -0400

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -1,6 +1,6 @@
 const RADIO_NAME = 'PRIMERA STEREO Radio Online';
-const URL_STREAMING = 'http://usa7.fastcast4u.com:5388/stream';
-const NOWPLAYING_TXT_URL = 'http://stream.zeno.fm/nowplaying.txt';
+const URL_STREAMING = 'http://usa7.fastcast4u.com:5388';
+const NOWPLAYING_TXT_URL = 'http://usa7.fastcast4u.com:5388/nowplaying.txt';
 const ZENO_API_URL = 'http://usa7.fastcast4u.com:5388/currentmetadata?sid=1';
 const API_KEY = "18fe07917957c289983464588aabddfb";
 const DEFAULT_COVER_ART = 'images/Nuevo Logo.png'; 
