1d5d2d7b4436df4da05e05b61bbbe0f2d55543f6
PRIMERA STEREO Radio Online <primerastereo2020@gmail.com>
2025-10-04 01:37:35 -0400

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -1,9 +1,9 @@
 const RADIO_NAME = 'PRIMERA STEREO Radio Online';
 const URL_STREAMING = 'https://usa7.fastcast4u.com/proxy/primerastereo/stream';
-const NOWPLAYING_TXT_URL = 'https://usa7.fastcast4u.com/proxy/primerastereo/nowplaying.txt';
-const ZENO_API_URL = 'https://usa7.fastcast4u.com/proxy/primerastereo/currentmetadata?sid=1';
+const NOWPLAYING_TXT_URL = 'https://usa7.fastcast4u.com/proxy/primerastereo/7.html';
+
 const API_KEY = "18fe07917957c289983464588aabddfb";
-const DEFAULT_COVER_ART = 'images/Nuevo Logo.png'; 
+const DEFAULT_COVER_ART = './images/Nuevo Logo.png'; 
 
 let showHistory = true;
 var audio = new Audio(URL_STREAMING);
