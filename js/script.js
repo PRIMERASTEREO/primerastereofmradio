d843dc2d30edf5abed8fe7f16d163d5bb940cea8
PRIMERA STEREO Radio Online <primerastereo2020@gmail.com>
2025-10-06 20:13:24 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const RADIO_NAME = 'PRIMERA STEREO Radio Online';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://usa7.fastcast4u.com/proxy/primerastereo/stream';
+const URL_STREAMING = 'http://usa7.fastcast4u.com:5388';
 
 // You can find the mount point in the Broadcast Settings.
 // To generate the Zeno Radio API link from the mount point,
@@ -9,7 +9,7 @@ const URL_STREAMING = 'https://usa7.fastcast4u.com/proxy/primerastereo/stream';
 // For example, if the mount point is 'https://usa7.fastcast4u.com/proxy/primerastereo',
 // the API link will be 'https://usa7.fastcast4u.com/proxy/primerastereo'.
 
-const url = 'https://usa7.fastcast4u.com/proxy/primerastereo';
+const url = 'http://usa7.fastcast4u.com:5388';
 
 // Visit https://api.vagalume.com.br/docs/ to get your API key
 const API_KEY = "18fe07917957c289983464588aabddfb";
@@ -538,6 +538,7 @@ function intToDecimal(vol) {
 function decimalToInt(vol) {
     return vol * 100;
 }
+
 
 
 
